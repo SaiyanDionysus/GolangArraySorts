9db4436369876ae89939f77faa0acd0648cd6a8b
githuber <denis>
2023-12-29 01:41:10 +0300

    bubbleSort

--- bubbleSort.go
+++ bubbleSort.go
@@ -6,7 +6,7 @@ import (
 )
 
 func BubbleSort(array []int) []int {
-	for i := 0; j < len(array)-1; i++ {
+	for i := 0; i < len(array)-1; i++ {
 		for j := 0; j < len(array)-i-1; j++ {
 			if array[j] > array[j+1] {
 				array[j], array[j+1] = array[j+1], array[j]
