a3ce4ede284ebc3e150876b11aac5085a0cd102e
githuber <denis>
2023-12-29 01:37:45 +0300

    bubbleSort

--- bubbleSort.go
+++ bubbleSort.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func BubbleSort(array []int) []int {
+	for i := 0; j < len(array)-1; i++ {
+		for j := 0; j < len(array)-i-1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+	return array
+}
+
+func main() {
+	array := rand.Perm(10)
+	fmt.Println("Your unsorted array: ", array)
+	fmt.Println("Your sorted array:", BubbleSort(array))
+}
